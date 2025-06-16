661a4238a23690b731d78297855820d2353696c7
Josephtur <turmeroro@gmail.com>
2025-06-16 17:21:35 +0200

    add other exercise

---
+++ 16jun/04ejercicio/script.js
@@ -0,0 +1,11 @@
+let numero = Number(prompt("Introduce el número:"));
+
+if (numero > 0) {
+    console.log("El número es positivo");
+} else if (numero < 0) {
+    console.log("El número es negativo");
+} else if (numero === 0) {
+    console.log("El número es cero");
+} else {
+    console.log("No has introducido un número válido");
+}
