4229c2cf0a0ec2ef876f3d4d67bc870eff8da66a
Josephtur <turmeroro@gmail.com>
2025-06-19 13:01:56 +0200

    ejercicios

---
+++ 19jun/ejercicios-propuestos/script.js
@@ -0,0 +1,64 @@
+// //Ejercicio 1 Factorial de un numero
+
+function factorial(n){
+ return 5!=5 * 4 * 3 * 2 * 1;
+
+}
+
+console.log(factorial(5*4*3*2*1===120));
+
+//Ejercicio sumar 1
+function incrementar(n) {
+  return n + 1;
+}
+
+console.log('El numero es:', incrementar(25));
+
+//Ejercicio multiplicar por 2
+function multiplicar (x) {
+  return x * 2;
+}
+
+console.log('El numero es:', multiplicar(30));
+
+//Ejercicio 3 restar
+function decrementar (n){
+    return n - 1;
+}
+console.log('El resultado es:', decrementar(10));
+
+//ejercicio 4 elevar al cuadrado
+
+function cuadrado(x){
+    return x * x;
+
+}
+console.log('resultado:',cuadrado(10));
+
+// 3ejercicio
+function celsiusAFahreinheit(c) {
+    return c * 9 / 5 + 32;
+
+}
+console.log('celsiusAFahrenheit(0) =', celsiusAFahreinheit(0)); //32
+console.log('celsiusAFahrenheit(100) =', celsiusAFahreinheit(100)); //212
+
+
+// ejercicio 7 booleano inverso
+function invertir(b) {
+    return !b;
+}
+console.log('invertir(true) =', invertir(true)); //false
+console.log('invertir(true) =', invertir(false)); // true
+
+// 9 elevar al cuadrado
+function cuadrado(x) {
+    return x *x;
+}
+console.log('cuadrado(5) =', cuadrado(10));
+
+//10 saludo
+function saludo (){
+    return '¡Hola mundo!';
+}
+console.log('saludo()=', saludo());
